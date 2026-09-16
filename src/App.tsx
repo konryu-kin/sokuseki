c26d16f33cc26380a91614e07c8ed8b0ffeebf2f
konryu <26-shomei@noreply.example.org>
2026-09-16 14:55:53 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <main>
+      <section>
+        <h2>過去</h2>
+        <div></div>
+      </section>
+      <section>
+        <h2>今日</h2>
+        <div>
+          <p>todoリストを作る</p>
+        </div>
+      </section>
+      <section>
+        <h2>明日</h2>
+        <div></div>
+      </section>
+      <section>
+        <h2>未来</h2>
+        <div></div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
