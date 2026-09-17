b297c10d8852374e2ebab0d84e02d42bc38ff1e9
konryu <26-shomei@noreply.example.org>
2026-09-18 08:40:40 +0900

    contextTagにevent生成ルールの型を追加

--- src/types/contextTag.ts
+++ src/types/contextTag.ts
@@ -2,4 +2,12 @@ export type ContextTag = {
   id: string;
   name: string;
   parentId: ContextTag["id"] | null;
+  generateEventRule?: {
+    eventTemplate: Event
+    options:{
+      // ミリ秒で指定
+      scheduledDateOffset?:number
+      dueDateOffset?:number
+    }
+  }
 };
