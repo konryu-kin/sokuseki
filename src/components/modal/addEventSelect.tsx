372b2372907a5c2851095df3a3acd167e83e3fae
konryu <26-shomei@noreply.example.org>
2026-09-20 18:17:02 +0900

    準備 必要なファイルを追加して、contextTagのgeneratedItemRulesを、①複数の選択肢をとれるようにして②選択肢の中でも複数のアイテムをまとめて追加できるようにした

---
+++ src/components/modal/addEventSelect.tsx
@@ -0,0 +1,7 @@
+export default function AddEventSelect(){
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/types/contextTag.ts
+++ src/types/contextTag.ts
@@ -1,13 +1,31 @@
+import type { Task } from "./task";
+import type { Event } from "./event";
+
 export type ContextTag = {
   id: string;
   name: string;
   parentId: ContextTag["id"] | null;
-  generateEventRule?: {
-    eventTemplate: Event
-    options:{
-      // ミリ秒で指定
-      scheduledDateOffset?:number
-      dueDateOffset?:number
-    }
-  }
+  generatedItemRules?: {
+    optionName: string;
+    rules: (
+      | {
+          type: "event";
+          ItemTemplate: Event;
+          options: {
+            // ミリ秒で指定
+            scheduledDateOffset?: number;
+            dueDateOffset?: number;
+          };
+        }
+      | {
+          type: "task";
+          ItemTemplate: Task;
+          options: {
+            // ミリ秒で指定
+            scheduledDateOffset?: number;
+            dueDateOffset?: number;
+          };
+        }
+    )[];
+  }[];
 };
